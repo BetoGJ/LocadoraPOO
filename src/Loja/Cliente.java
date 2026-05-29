d9eea6fad7f08703f474515e2da5e25ebdbfb5e3
thiagov09 <thiago.v0089@gmail.com>
2026-05-29 01:28:44 -0300

    fix names

--- src/Loja/Cliente.java
+++ src/Loja/Cliente.java
@@ -2,7 +2,7 @@ package Loja;
 
 
 public class Cliente extends Conta {
-    public Cliente(String nome, String cpf, String senha, String datadeNascimento) {
-        super(nome, cpf, senha, datadeNascimento);
+    public Cliente(String nome, String cpf, String senha, String dataDeNascimento) {
+        super(nome, cpf, senha, dataDeNascimento);
     }
 }
--- src/Loja/Vendedor.java
+++ src/Loja/Vendedor.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class Vendedor extends Conta{
     private Float salario;
     boolean isAdmin=false;
-    public Vendedor(String nome, String cpf, String senha, String datadeNascimento, Float salario) {
-        super(nome, cpf, senha, datadeNascimento);
+    public Vendedor(String nome, String cpf, String senha, String dataDeNascimento, Float salario) {
+        super(nome, cpf, senha, dataDeNascimento);
         this.salario = salario;
     }
 }
